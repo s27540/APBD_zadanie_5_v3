cf7f48301063a37f5fe86c0a19e6a63e216c577b
s27540 <s27540@pjwstk.edu.pl>
2024-04-15 19:15:22 +0200

    Inicjalizacja klasy AnimalService

---
+++ WebApplication1/WebApplication1/AnimalAPIStructure/AnimalService.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.AnimalAPIStructure;
+
+public class AnimalService : IAnimalService
+{
+    public List<Animal> GetAnimals(string orderBy)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int AddAnimal(Animal animal)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int UpdateAnimal(Animal animal, int idAnimal)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int DeleteAnimal(int idAnimal)
+    {
+        throw new NotImplementedException();
+    }
+}
