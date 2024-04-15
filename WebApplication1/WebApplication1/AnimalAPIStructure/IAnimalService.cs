1a4b92db85e53c7665f30ab9f4c8094cadee2019
s27540 <s27540@pjwstk.edu.pl>
2024-04-15 19:11:09 +0200

    Dodanie intrrfejsu IAnimalService

---
+++ WebApplication1/WebApplication1/AnimalAPIStructure/IAnimalService.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.AnimalAPIStructure;
+
+public interface IAnimalService
+{
+    public List<Animal> GetAnimals(string orderBy);
+
+    public int AddAnimal (Animal animal);
+
+    public int UpdateAnimal (Animal animal, int idAnimal);
+
+    public int DeleteAnimal ( int idAnimal );
+
+}
