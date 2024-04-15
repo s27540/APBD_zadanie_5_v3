c9114345c828b7c341193539e01a80764222e4a4
s27540 <s27540@pjwstk.edu.pl>
2024-04-15 19:23:03 +0200

    Uzupełnienie metody GetAnimal w klasie AnimalService

--- WebApplication1/WebApplication1/AnimalAPIStructure/AnimalService.cs
+++ WebApplication1/WebApplication1/AnimalAPIStructure/AnimalService.cs
@@ -1,10 +1,60 @@
-﻿namespace WebApplication1.AnimalAPIStructure;
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.AnimalAPIStructure;
 
 public class AnimalService : IAnimalService
 {
+    private readonly string _sqlConnection = "Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=2019SBD;Integrated Security=True";
     public List<Animal> GetAnimals(string orderBy)
     {
-        throw new NotImplementedException();
+        string sql = "SELECT * FROM ANIMAL";
+        var output = new List<Animal>();
+
+        if (string.IsNullOrEmpty(orderBy))
+        {
+            orderBy = "name";
+        }
+
+        using (var connection = new SqlConnection(_sqlConnection))
+        {
+            using (var comm = new SqlCommand())
+            {
+                comm.Connection = connection;
+
+                if (orderBy == "name" || orderBy == "description" || orderBy == "area" || orderBy == "category")
+                {
+
+                    comm.CommandText = $"{sql} ORDER BY {orderBy} ASC";
+
+                    connection.Open();
+
+                    var reader = comm.ExecuteReader();
+
+                    if (!reader.HasRows)
+                    {
+                        Console.WriteLine("Empty rows");
+                    }
+
+                    while (reader.Read())
+                    {
+                        output.Add( new Animal
+                        {
+                            IdAnimal = int.Parse( reader["IdAnimal"].ToString() ),
+                            Name = reader["Name"].ToString(),
+                            Description = reader["Description"].ToString(),
+                            Category = reader["Category"].ToString(),
+                            Area = reader["Area"].ToString()
+
+                        } );
+                    }
+                }
+
+
+                connection.Close();
+            }
+        }
+
+        return output;
     }
 
     public int AddAnimal(Animal animal)
